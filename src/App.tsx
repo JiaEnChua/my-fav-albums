acef3a059e3b7cd0576b79c6209c49ee232eaf83
jiaenchua <jiaen_je@hotmail.com>
2021-02-22 20:24:09 -0600

    remove app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
 import Home from './components/Home';
 import NotFound from './components/NotFound';
 
 const App: React.FC = () => {
   return (
-    <div className='app'>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
